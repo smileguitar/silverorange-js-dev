98059ef2fdf834bb285ab3c9e9dea396c6977f42
smileguitar <luansupwork@outlook.com>
2021-12-09 23:16:24 +0700

    first commit

---
+++ api/src/app.ts
@@ -0,0 +1,32 @@
+import express, { Response, Request, NextFunction } from 'express';
+import cors from 'cors';
+import { repos } from './routes/repos';
+import { terrible } from './middleware/terrible';
+import { AppError } from './typings/AppError';
+
+// CORS header configuration
+const corsOptions = {
+  methods: 'GET',
+  allowedHeaders: 'Content-Type,Authorization',
+};
+
+export const app = express();
+
+// Routes. Note these will fail about 25% due to "terrible" middleware.
+app.use('/repos', terrible(), cors(corsOptions), repos);
+
+// error handling middleware should be loaded after the loading the routes
+app.use(
+  '/',
+  (err: AppError, req: Request, res: Response, next: NextFunction) => {
+    const status = err.status || 500;
+
+    const formattedError: { status: number; message: string } = {
+      status,
+      message: err.message,
+    };
+
+    res.status(status);
+    res.send(JSON.stringify(formattedError));
+  }
+);
