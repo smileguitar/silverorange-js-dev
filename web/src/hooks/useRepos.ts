22bc9d2c7d5ca2ae5a2140da76c5ba8f9226e16c
smileguitar <luansupwork@outlook.com>
2021-12-10 22:18:45 +0500

    [web] added language filter

--- web/src/hooks/useRepos.ts
+++ web/src/hooks/useRepos.ts
@@ -3,9 +3,13 @@ import { SERVER_ENDPOINT } from 'consts';
 import { Repo } from 'types/Repo';
 import dayjs from 'dayjs';
 
-export default function useRepos() {
+interface UseReposType {
+  languageFilter: string;
+}
+
+export default function useRepos({ languageFilter }: UseReposType) {
   // original data recieved from server
-  const [, setOriginalData] = useState<Repo[]>([]);
+  const [originalData, setOriginalData] = useState<Repo[]>([]);
   // sorted/filterd data that is displayed to user
   const [data, setData] = useState<Repo[]>([]);
   const [isFetching, setIsFetching] = useState(true);
@@ -16,23 +20,30 @@ export default function useRepos() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (!languageFilter || languageFilter === 'All') {
+      setData(originalData);
+    } else {
+      setData(originalData.filter((item) => item.language === languageFilter));
+    }
+  }, [languageFilter, originalData]);
+
   const fetchData = async () => {
     try {
       const returnData = await fetch(`${SERVER_ENDPOINT}/repos`);
       const reposData = await returnData.json();
       if (returnData.status === 200) {
-        setOriginalData(reposData);
-        setData(
-          reposData.sort((a: Repo, b: Repo) => {
-            if (dayjs(a.created_at).isSame(b.created_at)) {
-              return 0;
-            }
-            if (dayjs(a.created_at).isBefore(b.created_at)) {
-              return -1;
-            }
-            return 1;
-          })
-        );
+        const orderedData = reposData.sort((a: Repo, b: Repo) => {
+          if (dayjs(a.created_at).isSame(b.created_at)) {
+            return 0;
+          }
+          if (dayjs(a.created_at).isBefore(b.created_at)) {
+            return -1;
+          }
+          return 1;
+        });
+        setOriginalData(orderedData);
+        setData(orderedData);
         setIsError(false);
       } else {
         setOriginalData([]);
@@ -52,5 +63,5 @@ export default function useRepos() {
     fetchData();
   }, []);
 
-  return { data, isFetching, isError, reFetch };
+  return { data, originalData, isFetching, isError, reFetch };
 }
--- web/src/views/ReposView.tsx
+++ web/src/views/ReposView.tsx
@@ -1,18 +1,69 @@
 import useRepos from 'hooks/useRepos';
+import { useEffect, useState } from 'react';
 import './ReposView.css';
 
 export default function ReposView() {
-  const { data: reposData, isError, isFetching, reFetch } = useRepos();
+  const [selectedLanguage, setSelectedLanguage] = useState<string>('All');
+
+  const {
+    data: reposData,
+    originalData,
+    isError,
+    isFetching,
+    reFetch,
+  } = useRepos({
+    languageFilter: selectedLanguage,
+  });
+
+  const [languageList, setLanguageList] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (originalData && originalData.length > 0) {
+      const list = originalData
+        .map((repo) => repo.language)
+        .sort()
+        .filter((item, pos, ary) => {
+          return !pos || item !== ary[pos - 1];
+        });
+      list.unshift('All');
+      setLanguageList(list);
+    } else {
+      setLanguageList([]);
+    }
+  }, [originalData]);
+
   return (
     <div className="container">
       {isFetching && <p>Loading Repos...</p>}
       {!isFetching && isError && (
         <>
           <p className="error_msg">Network error occured.</p>
-          <button onClick={reFetch}>Retry</button>
+          <button className="btn" onClick={reFetch}>
+            Retry
+          </button>
+        </>
+      )}
+      {!isFetching && !isError && (
+        <>
+          <div className="language_filter_container">
+            <p>Filter by language: </p>
+            {languageList.map((language) => (
+              <button
+                className="btn"
+                onClick={() => setSelectedLanguage(language)}
+                key={language}
+              >
+                {language}
+              </button>
+            ))}
+          </div>
+          {selectedLanguage !== 'All' && (
+            <p>Active filter: {selectedLanguage}</p>
+          )}
         </>
       )}
       {!isFetching &&
+        !isError &&
         reposData.map((repo) => (
           <div className="repo_card" key={repo.id}>
             <p>name: {repo.name}</p>
