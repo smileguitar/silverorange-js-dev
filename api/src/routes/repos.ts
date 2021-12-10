e7b1eeece3a23e6628b2d5ca0e5900dc046721ec
smileguitar <luansupwork@outlook.com>
2021-12-10 18:52:17 +0500

    [feat] Implemented repos route

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,21 @@
+import axios, { AxiosResponse } from 'axios';
 import { Router, Request, Response } from 'express';
+import { Repo } from '../typings/Repo';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
-  res.status(200);
-
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  try {
+    const returnData: AxiosResponse<Repo[]> = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    const filteredData = returnData.data.filter((item) => !item.fork);
+    res.status(200);
+    res.json({ status: 200, data: filteredData });
+  } catch (e) {
+    res.status(400);
+    res.json({ status: 400, message: 'Failed to fetch data.' });
+  }
 });
